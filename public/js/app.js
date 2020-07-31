f2cc3fdfdb13e665946a3d317970a664a143b65e
Jitendra Oza <jeet1293@users.noreply.github.com>
2020-07-31 20:13:52 +0530

    heroku changes

--- public/js/app.js
+++ public/js/app.js
@@ -37119,8 +37119,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\xampp74\htdocs\lara7\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\xampp74\htdocs\lara7\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\xampp\htdocs\adminlara\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\xampp\htdocs\adminlara\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
